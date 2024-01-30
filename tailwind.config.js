29a7a0ff472a41fe0c006d82fbc705d6b84166cb
Aurelius Wendelken <webtobesocial@gmail.com>
2024-01-31 00:46:16 +0100

    fixed tailwind config to extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,17 @@
 module.exports = {
   content: ['./src/*.{html,js}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'oddblue': '#003c9e',
-      'oddlightgreen': '#b0ed82',
-      'oddlightblue': '#00d1ff',
-      'oddpurple': '#85abfd',
-      'oddlime': '#dbfe55',
-      'oddmagenta': '#db95f7',
-      'odddarkgray': '#1a1a1a'
+    extend: {
+      colors: {
+        'oddblue': '#003c9e',
+        'oddlightgreen': '#b0ed82',
+        'oddlightblue': '#00d1ff',
+        'oddpurple': '#85abfd',
+        'oddlime': '#dbfe55',
+        'oddmagenta': '#db95f7',
+        'odddarkgray': '#1a1a1a',
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
