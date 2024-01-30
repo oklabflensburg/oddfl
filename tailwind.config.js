1fc91785efac455289e5d03923f7bef6ac02937f
Aurelius Wendelken <webtobesocial@gmail.com>
2024-01-31 00:32:53 +0100

    first draft using parcel build process

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.{html,js}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'oddblue': '#003c9e',
+      'oddlightgreen': '#b0ed82',
+      'oddlightblue': '#00d1ff',
+      'oddpurple': '#85abfd',
+      'oddlime': '#dbfe55',
+      'oddmagenta': '#db95f7',
+      'odddarkgray': '#1a1a1a'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
